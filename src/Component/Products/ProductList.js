156c531c34fb80c4afec4a06b4b8913488c0847d
ana almonacid <anaalmonacid1897@gmail.com>
2022-10-26 19:36:00 -0300

    so1

--- src/Component/Products/ProductList.js
+++ src/Component/Products/ProductList.js
@@ -1,29 +1,33 @@
-import { useContext, useState } from "react";
+import React, { useContext } from 'react'
+import { DataContext } from '../Context/DataProvider'
+import Products from './Products'
+
 
 const ProductList = () => {
 
-  const value = useContext(DataProvider)
-  const [products] = value.products
-  console.log(products)
+    const value = useContext(DataContext)
+    const [products] = value.products
+    console.log(products)
 
-return (        
-  <div>
-      <h1 className='ProductsTitle'>PRODUCTOS</h1>
-      <div className='ProductsContainer'>
-          {
-              products.map(product => (
-              <Products
-              key={product.id}
-              id={product.id}
-              title={product.title}
-              price={product.price}
-              image={product.image}
-              detail={product.detail}
-              stock={product.stock}
-              />))
-          }
-      </div>
-  </div>
-)}
+  return (        
+    <div>
+        <h1 className='ProductsTitle'>PRODUCTOS</h1>
+        <div className='ProductsContainer'>
+            {
+                products.map(product => (
+                <Products
+                key={product.id}
+                id={product.id}
+                title={product.title}
+                price={product.price}
+                image={product.image}
+                detail={product.detail}
+                stock={product.stock}
+                />))
+            }
+        </div>
+    </div>
+  )
+}
 
 export default ProductList
